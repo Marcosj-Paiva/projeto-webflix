6240234722228d5654e07c438bf4f67597c749de
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2022-07-21 21:58:05 -0300

    up projeto

---
+++ src/scripts/model/modelRequest.js
@@ -0,0 +1,14 @@
+function Movies( title ,sinopse, image ,) {
+    let title ;
+    return {
+        title : this.title,
+        sinopse : this.sinopse,
+        image : this.image
+    }
+}
+
+
+
+ let filmes = new Movies(title = "madagascar")
+
+ console.log(filmes)
